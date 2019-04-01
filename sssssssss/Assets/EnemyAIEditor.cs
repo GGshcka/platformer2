0ec1279e2377c813e8c39ae4ef7b5409156ab114
GGshcka <46972517+GGshcka@users.noreply.github.com>
2019-04-01 21:05:36 +0300

    + Anim

---
+++ sssssssss/Assets/EnemyAIEditor.cs
@@ -0,0 +1,23 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(EnemyAI_))]
+
+public class EnemyAIEditor : Editor
+{
+
+    public GameObject waypoint; // префаб вейпоинта
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        EnemyAI_ t = (EnemyAI_)target;
+        if (GUILayout.Button("Generate Waypoints"))
+        {
+            t.GenerateWaypoints(waypoint);
+        }
+    }
+}
+#endif
